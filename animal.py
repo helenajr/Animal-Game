ad797b0edd7504643cc71f888718969a13f3a79d
Kevon Williams <KevonReuWilliams@gmail.com>
2024-11-30 15:49:54 -0500

    Add goat to animal list

--- animal.py
+++ animal.py
@@ -5,14 +5,16 @@ import sys
 
 # setup the game by storing animals in a list and their dietry habits in a dictionary
 animal_list = ["dog", "cat", "porcupine", "whale", "rat", "rabbit", "pig", "horse","lion"]
-animal_food = {"dog":["beef","chicken","rice"], "cat":["mice","eggs","cereal"],
+animal_food = {"dog":["beef","chicken","rice"],
+               "cat":["mice","eggs","cereal"],
                "porcupine": ["worms","beetles","ants"] , 
                "whale":["prawns", "fish","seals"], 
                "rat":["corn","fruit","biscuits"],
                "rabbit":["carrots","lettuce","nuts"],
                "pig":["potatoes","turnips","cabbage"],
                "horse":["hay", "sugar cubes"],
-               "lion":["deer", "buffalo","zebra"]}
+               "lion":["deer", "buffalo","zebra"],
+               "goat":["grass","leaves","bark"]}
 health_bar = 100 # size of heath bar which is printed to console
 
 # start the game by randomly selecting an animal and a health score
